ca8e9f4551bd160cc73ecd11fbb193d5a16168fb
hcx07 <756170593@qq.com>
2018-08-15 17:41:14 +0800

    12

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -25,7 +25,6 @@ return [
         'session' => [
             // this is the name of the session cookie used for login on the frontend
             'name' => 'advanced-frontend',
-            'savePath'=>'/tmp',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
