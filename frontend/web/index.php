ca61a140bae72fa260a6dbb8254e554c8c656140
hcx07 <756170593@qq.com>
2018-08-20 17:17:07 +0800

    main

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -19,13 +19,4 @@ $config = yii\helpers\ArrayHelper::merge(
 (new yii\web\Application($config))->run();
 
 ?>
-<script>
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?6de0f0860f70302c41a63a4992da7ad8";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-</script>
 
