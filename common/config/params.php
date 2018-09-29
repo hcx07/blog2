cbfc5022923a62f1504032c880b88f8e5ee11205
hcx07 <756170593@qq.com>
2018-09-29 18:01:02 +0800

    123

--- common/config/params.php
+++ common/config/params.php
@@ -7,6 +7,6 @@ return [
         'accessKey'=>'zt7cYHq5mPq7-UAM8zr7losB03xUBQsOC15KYgKA',
         'secretKey'=>'h04GEOs7cwKVoA0SjZ5GLF_pT1-QPUjq-qunCUpz',
         'bucket'=>'blog',
-        'url'=>'http://pft1t09t2.bkt.clouddn.com/'
+        'url'=>'http://file.muniao.org/'
     ],
 ];
