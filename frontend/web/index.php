e0a53de562682ea60689faf7600a8c942df12368
hcx <756170593@qq.com>
2018-12-24 10:42:26 +0800

    * 日志

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -23,7 +23,7 @@ $fp = fopen("./log/$h.txt", "a+");
 $url=Yii::$app->request->getHostInfo().Yii::$app->request->url;
 $ip=Yii::$app->request->getUserIP();
 $HTTP_REFERER=array_key_exists('HTTP_REFERER',$_SERVER)?$_SERVER['HTTP_REFERER']:'';
-fwrite($fp, date("Y-m-d H:i:s") . ' url:' . $url .' ip:'.$ip.' HTTP_REFERER:'.$HTTP_REFERER.' browser:'.$log->GetBrowser().' lang:'.$log->GetLang().'os:'.$log->GetOs() ."\r\n");
+fwrite($fp, date("Y-m-d H:i:s") . ' url:' . $url .' ip:'.$ip.' HTTP_REFERER:'.$HTTP_REFERER.' browser:'.$log->GetBrowser().'os:'.$log->GetOs().'HTTP_USER_AGENT:'.$_SERVER['HTTP_USER_AGENT'] ."\r\n");
 fclose($fp);
 ?>
 <script>
